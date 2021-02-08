d576a61d5f4a7b55dba20b7f3c6b60ce9a764ff7
GeorgiPangev <666666999ui>
2021-02-08 21:05:07 +0200

    Added tasks

---
+++ Multiplicatione1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Multiplicatione1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (y <=10)
+            {  
+                for (int i = y; i <= 10; i++)
+                {
+                Console.WriteLine($"{n} X {i} = {n*i}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{n} X {y} = {n * y}");
+            }
+        }
+    }
+}
