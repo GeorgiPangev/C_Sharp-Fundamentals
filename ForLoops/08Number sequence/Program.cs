9ad18861f9d9d96c0e085b67750e1db9582e35dd
Georgi Pangev <PangevG@Gmail.com>
2021-02-21 22:33:50 +0200

    For Loops

---
+++ ForLoops/08Number sequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _08Number_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeets = int.Parse(Console.ReadLine());
+          
+
+            int maxNum = int.MinValue;
+                int minNum = int.MaxValue;
+
+            for (int i = 1; i <= repeets; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number > maxNum)
+                {
+                    maxNum = number;
+                }
+                if (number < minNum)
+                {
+                    minNum = number;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNum}");
+            Console.WriteLine($"Min number: {minNum}");   
+        }
+    }
+}
---
+++ ForLoops/09LeftandRightSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _09LeftandRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeets = int.Parse(Console.ReadLine());
+
+            int lSum = 0;
+            int rSum = 0;
+
+            for (int i = 1; i <= repeets ; i++)
+            {
+                int curentNumb = int.Parse(Console.ReadLine());
+                lSum += curentNumb;
+            }
+            for (int i = 1; i <=repeets ; i++)
+            {
+                int curentNumb = int.Parse(Console.ReadLine());
+                rSum += curentNumb;
+            }
+            if (lSum == rSum)
+            {
+                Console.WriteLine($"Yes, sum = {lSum}");
+            }
+            if (lSum != rSum)
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(lSum-rSum)}");
+            }
+        }
+    }
+}
