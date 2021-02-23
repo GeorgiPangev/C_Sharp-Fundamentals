ec427de791acb0560026d935d0d4237bb1037907
Georgi Pangev <PangevG@Gmail.com>
2021-02-23 22:16:00 +0200

    +2

---
+++ ForLoops/NumSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeets = int.Parse(Console.ReadLine());
+            
+            int sum = 0;
+            for (int i = 1; i <= repeets ; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;  
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
