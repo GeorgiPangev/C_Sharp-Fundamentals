731c3729c2547697a1bce71b05af491564cf5e6d
Georgi Pangev <PangevG@Gmail.com>
2021-02-26 22:06:25 +0200

    ++

---
+++ ForLoops/ForLoopsExercise/HalfSum lement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _02.HalfSum_lement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int max = int.MinValue;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+                if (num > max)
+                {
+                    max = num;
+                }
+
+            }
+            int sumWitoutMax = sum - max;
+            if (sumWitoutMax == max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {max}");
+            }
+            else/* if (sumWitoutMax != max)*/
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sumWitoutMax - max)}");
+            }
+        }
+    }
+}
