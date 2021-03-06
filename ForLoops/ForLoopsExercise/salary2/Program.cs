f34f7f1a0a4a8ab8048e0b469780092069b85c18
Georgi Pangev <PangevG@Gmail.com>
2021-03-06 19:16:16 +0200

    new two added.

---
+++ ForLoops/ForLoopsExercise/salary2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _7Selary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double tabs = Double.Parse(Console.ReadLine());
+            Double salary = Double.Parse(Console.ReadLine());
+
+            for (double i = 1; i <= tabs; i++)
+            {
+                if (salary <= 0)
+                {
+                    tabs = 0;
+                    Console.WriteLine("You have lost your salary.");
+                }
+                
+                else if (salary > 0)
+                {
+                    string sites = Console.ReadLine();
+                    switch (sites)
+                    {
+                        case "Facebook": salary -= 150;
+                            if (salary <= 0)
+                            {
+                                tabs = 0;
+                                Console.WriteLine("You have lost your salary.");
+                            }
+                            break;
+                        case "Instagram": salary -= 100; 
+                            if (salary <= 0)
+                            {
+                                tabs = 0;
+                                Console.WriteLine("You have lost your salary.");
+                            }
+                            break;
+                        case "Reddit": salary -= 50;
+                            if (salary <= 0)
+                            {
+                                tabs = 0;
+                                Console.WriteLine("You have lost your salary.");
+                            }
+                            break;
+                    }
+                }
+               
+            }
+           
+            if (salary > 0)
+            {
+                Console.WriteLine(Math.Round(salary));
+            }
+            //else if (salary <= 0)
+            //{
+
+            //    Console.WriteLine("You have lost your salary.");
+            //}
+        }
+    }
+}
