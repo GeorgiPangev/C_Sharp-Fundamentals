41e661874d0b5a0fbdb26c30302fb7b79643b0a9
Georgi Pangev <PangevG@Gmail.com>
2021-03-14 17:18:22 +0200

    string problems.

---
+++ Metods/Lab/RepeatString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            string toPrintStr = StrByN(input, n);
+            Console.WriteLine(toPrintStr);
+        }
+        private static string StrByN(string input, int n)
+        {
+            StringBuilder done = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                done.Append(input);
+                
+               
+
+            }
+            return done.ToString();
+
+        }
+
+       
+    }
+}
---
+++ Metods/Lab/SingOfIntegerNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SingOfIntegerNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Print(num);
+        }
+
+        private static void Print(int number)
+        {
+            if (number<0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+        }
+    }
+}
