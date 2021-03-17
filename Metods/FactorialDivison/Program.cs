c9e995b13ead21f8e8350b3f4dc2f7e277907413
Georgi Pangev <PangevG@Gmail.com>
2021-03-17 16:10:40 +0200

    Plus

---
+++ Metods/FactorialDivison/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FactorialDivison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first = double.Parse(Console.ReadLine());
+            double second = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{FactorelCount(first)/FactorelCount(second):F2} ");
+
+        }
+        private static double FactorelCount(double x)
+        {
+            double calculecion = x;
+
+            for (double i = x-1; i >= 1; i--)
+            {
+                calculecion *= i;
+            }
+            return calculecion;
+        
+
+        }
+    }
+}
---
+++ Metods/MedleCharacter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace MedleCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintMidel(input);
+               
+        }
+        private static void PrintMidel(string abc)
+        {
+            int midLeng = abc.Length;
+            if (abc.Length%2!=0)
+            {
+                midLeng = ((abc.Length - 1) / 2) ;
+                Console.WriteLine(abc[midLeng]);
+            }
+            else
+            {
+                midLeng = (abc.Length / 2)-1;
+                Console.WriteLine($"{abc[midLeng]}{abc[midLeng+1]}");
+            }
+        }
+    }
+}
