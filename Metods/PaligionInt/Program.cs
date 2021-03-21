ae0a43ae84f146ca18db15741843be1d6f41cbb1
Georgi Pangev <PangevG@Gmail.com>
2021-03-21 21:58:06 +0200

    ++

---
+++ Metods/PaligionInt/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+
+namespace PaligionInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input !="END")
+            {
+
+         
+            int[] arr = (int[])ConvertToArray(input);
+
+                if (IsOrNot(arr))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            ;
+            input = Console.ReadLine();
+
+            }
+
+
+
+        }
+
+        private static bool IsOrNot(int[] arr)
+        {
+            bool IsDifrent = false;
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i]!=arr[(arr.Length-1)-i])
+                {
+                    IsDifrent = true;
+                }
+            }
+            if (IsDifrent)
+            {
+                return false;
+
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private static Array ConvertToArray(string input)
+        {
+            int[] array = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                array[i] = int.Parse(input[i].ToString());
+
+            }
+            return array;
+        }
+    }
+}
