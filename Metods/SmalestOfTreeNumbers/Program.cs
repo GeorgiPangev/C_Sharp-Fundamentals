33136e024de16f979a28cfd25a4f1d8fab46a491
Georgi Pangev <PangevG@Gmail.com>
2021-03-22 23:06:08 +0200

    Plus two.

---
+++ Metods/SmalestOfTreeNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SmalestOfTreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int one = int.Parse(Console.ReadLine());
+            int two = int.Parse(Console.ReadLine());
+            int tree = int.Parse(Console.ReadLine());
+
+            LowestNumb(one,two, tree);
+        }
+
+        private static void LowestNumb(int one, int two, int tree)
+        {
+            int LolNumberOneTwo = Math.Min(one, two);
+            int LolestNumb = Math.Min(LolNumberOneTwo, tree);
+            Console.WriteLine(LolestNumb);
+            //return LolestNumb;
+        }
+    }
+}
