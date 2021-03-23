6401a60d2be398bd8d3a97dc304efae923fedba4
Georgi Pangev <PangevG@Gmail.com>
2021-03-23 18:27:09 +0200

    Dir. chenges

---
+++ Metods/Exercise/passValidator/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace passValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine()
+                .ToLower();
+            bool islenghtRigt = LenghtCheck(input);
+            bool IsLetersAndDigitsOnli = LethrsDigCheck(input);
+            bool IsTwoDigitIn = DigitCheck(input);
+
+            if (!islenghtRigt)
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+            if (!IsLetersAndDigitsOnli)
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+            if (!IsTwoDigitIn)
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+            if (islenghtRigt&& IsLetersAndDigitsOnli&& IsTwoDigitIn)
+            {
+                Console.WriteLine("Password is valid");
+            }
+
+        }
+
+        private static bool DigitCheck(string input)
+        {
+            int sumOfDigit = 0;
+            foreach (var item in input)
+            {
+                if (char.IsDigit(item))
+                {
+                    sumOfDigit++;
+                }
+            }
+            if (sumOfDigit>=2)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool LethrsDigCheck(string input)
+        {
+            bool isNotinRange = true;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(input[i]))
+                {
+                    isNotinRange = false;
+                }
+               
+                
+            }
+            return isNotinRange;
+        }
+
+        private static bool LenghtCheck(string input)
+        {
+            if (input.Length>=6&& input.Length<=10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Metods/Exercise/problem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] arr = str
+               //.Replace('', ' ')
+
+                .Split("")
+               // .Select(int.Parse)
+                //.Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(string.Join("+",arr));
+            Console.WriteLine(arr.Length);
+        }
+    }
+}
