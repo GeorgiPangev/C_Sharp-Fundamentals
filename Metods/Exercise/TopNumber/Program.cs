67b132d6edeaecfc051fca8ac4bb6735acd40d26
Georgi Pangev <PangevG@Gmail.com>
2021-03-24 16:29:55 +0200

    Two more & problem descr.

---
+++ Metods/Exercise/TopNumber/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            StringBuilder str = new StringBuilder();
+            string[] topNums = DefindingInStr(x, str)
+                .Split(" ")
+                .ToArray();
+            PrintArr(topNums);
+
+
+        }
+
+        private static void PrintArr(string[] topNums)
+        {
+            foreach (var item in topNums)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static string DefindingInStr(int n, StringBuilder curentStr)
+        {
+            curentStr = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+               
+                int sumofdigits = 0;
+                bool isOddDigit = false;
+                int digit = i;
+                while (digit>=1)
+                {
+                    int curDigit = digit% 10;
+
+                    sumofdigits += curDigit;
+                    if (curDigit%2!=0)
+                    {
+                        isOddDigit = true;
+                    }
+
+                    digit /= 10;
+
+                }
+                if (isOddDigit&& sumofdigits%8==0)
+                {
+                    curentStr.Append(i + " ");
+                    
+                }
+            }
+            return curentStr.ToString();
+        }
+    }
+}
---
+++ Metods/Exercise/VowelCount/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace VowelCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string instr = Console.ReadLine()
+                .ToLower();
+               
+           
+            Console.WriteLine(VovelCounter(instr));
+
+
+        }
+
+        private static int VovelCounter(string input)
+        {
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                
+                if ("aeiou".Contains(input[i].ToString()))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
