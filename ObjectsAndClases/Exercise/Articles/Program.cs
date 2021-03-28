6b8f9af57549550ec0a0e5d93e629cfe41f1161b
Georgi Pangev <PangevG@Gmail.com>
2021-03-28 22:22:47 +0300

    Exsercise aded.

---
+++ ObjectsAndClases/Exercise/Articles/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Articles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                 .Split(", ")
+                 .ToArray();
+
+            Articles art = new Articles(input[0], input[1], input[2]);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] comand = Console.ReadLine()
+                    .Split(": ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                switch (comand[0])
+                {
+                    case "Edit":
+                        art.ChangeContent(comand[1]);
+                        break;
+                    case "ChangeAuthor":
+                        art.ChangeAutor(comand[1]);
+                        break;
+                    case "Rename":
+                        art.ChangeTitle(comand[1]);
+                        break;
+                }
+
+            }
+            Console.WriteLine(art.ToString());
+        }
+
+      
+    }
+    class Articles
+    {
+
+        public Articles(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+
+            Author = author;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public  void ChangeContent(string v)
+        {
+            Content = v;
+        }
+        public void ChangeAutor(string or)
+        {
+            Author = or;
+        }
+        public void ChangeTitle(string title)
+        {
+            Title = title;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
---
+++ ObjectsAndClases/Exercise/Articles2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Articles2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Articles> arts = new List<Articles>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(", ")
+                    .ToArray();
+                Articles artNew = new Articles(input[0], input[1], input[2]);
+                arts.Add(artNew);
+            }
+            string comand = Console.ReadLine();
+
+            switch (comand)
+            {
+                case "title":
+                    arts.Sort((t1,t2)=> t1.Title.CompareTo(t2.Title));
+   
+                    break;
+
+                case "content":
+                    arts = arts.OrderBy(c => c.Content).ToList();
+                    break;
+                case "autor":
+                    arts.Sort((a1, a2) => a1.Autor.CompareTo(a2.Autor));
+                    break;
+
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, arts));
+                       
+        }
+    }
+    class Articles
+    {
+        public Articles(string title, string content, string autor)
+        {
+            Title = title;
+            Content = content;
+            Autor = autor;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Autor { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} {Content} {Autor}"; 
+        }
+    }
+}
