7fdab74685f64a29897457c0c835ebedb9c204f3
Georgi Pangev <PangevG@Gmail.com>
2021-03-29 22:34:16 +0300

    Added two.

---
+++ ObjectsAndClases/Exercise/CarCatalog/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarCatalog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            List<Catalog> veahicle = new List<Catalog>();
+
+            while (input[0] != "End")
+            {
+                Catalog newCar = new Catalog(input[0], input[1], input[2], 
+                    double.Parse(input[3]));
+
+
+                veahicle.Add(newCar);
+
+                input = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            }
+            
+            string modelToprint = Console.ReadLine();
+
+            while (modelToprint != "Close the Catalogue")
+            {
+                //int index = veahicle.FindIndex(x => x.Model == modelToprint);
+
+                //Console.WriteLine(veahicle[index]);
+                Catalog pritCar = veahicle.First(x => x.Model == modelToprint);
+                Console.WriteLine(pritCar);
+
+                modelToprint = Console.ReadLine();
+            }
+
+            Catalog[] cars = veahicle.Where(x => x.Type == "car")
+               .ToArray();
+            List<Catalog> trucs = veahicle.Where(z => z.Type == "truck")
+              .ToList();
+            double truckSum = 0;
+            double carsum = 0;
+            double carsCout = 0;
+
+            foreach (var carTruck in veahicle)
+            {
+                if (carTruck.Type == "car")
+                {
+                    carsum += carTruck.HP;
+                    carsCout++;
+
+                }
+                else if (carTruck.Type == "truck")
+                {
+                    truckSum += carTruck.HP;
+                    
+                }
+            }
+
+            if (cars.Length >0)
+            {
+                Console.WriteLine($"Cars have average horsepower of: {(carsum / carsCout):F2}.");
+            }
+            else
+            {
+                Console.WriteLine($"Cars have average horsepower of: 0.00.");
+            }
+            if (trucs.Count >0)
+            {
+                Console.WriteLine($"Trucks have average horsepower of: {(truckSum / (veahicle.Count - carsCout)):F2}.");
+            }
+            else
+            {
+                Console.WriteLine($"Trucks have average horsepower of: 0.00.");
+            }
+           
+
+
+            //foreach (var truck in trucs)
+            //{
+
+            //    truckSum += truck.HP;
+            //}
+            //foreach (var car in cars)
+            //{
+            //    carsum += car.HP;
+            //}
+
+
+           
+            
+
+           // Console.WriteLine($"Cars have average horsepower of: {(carsum / cars.Length):F2}.");
+           //Console.WriteLine($"Trucks have average horsepower of: {(truckSum / trucs.Count):F2}.");
+        }
+    }
+    class Catalog
+    {
+        public Catalog(string type, string model, string color, double hp)
+        {
+            Type = type;
+            Model = model;
+            Color = color;
+            HP = hp;
+
+        }
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double HP { get; set; }
+        
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Type: {(Type == "car" ? "Car" : "Truck")} ");
+            sb.AppendLine($"Model: {Model}");
+            sb.AppendLine($"Color: {Color}");
+            sb.AppendLine($"Horsepower: {HP}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ ObjectsAndClases/Exercise/ObjectsAndClasesExsercise/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ObjectsAndClasesExsercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[]  phrases = { "Excellent product.",
+                    "Such a great product.",
+                    "I always use that product.",
+                    "Best product of its category.",
+                    "Exceptional product.",
+                    "I can’t live without this product."};
+
+            string[] events = { "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!"};
+
+            string[] authors = { "Diana",
+                "Petya",
+                "Stella",
+                "Elena",
+                "Katya",
+                "Iva",
+                "Annie",
+                "Eva"};
+
+            string[] cities = { "Burgas",
+                "Sofia",
+                "Plovdiv",
+                "Varna",
+                "Ruse"};
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Random ph = new Random();
+                int p = ph.Next(0, phrases.Length);
+
+                Random ev = new Random();
+                int e= ev.Next(0, events.Length);
+
+                Random au = new Random();
+                int a = au.Next(0, authors.Length);
+
+                Random ci = new Random();
+                int c = ci.Next(0, cities.Length);
+
+
+                Console.WriteLine($"{phrases[p]} {events[e]} {authors[a]} – {cities[c]}.");
+            }
+
+
+           
+
+        }
+    }
+}
