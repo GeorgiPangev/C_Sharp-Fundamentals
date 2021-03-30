e468660edab7c07142d6c219ae0d16a06cc62fd2
Georgi Pangev <PangevG@Gmail.com>
2021-03-30 22:42:46 +0300

    ++

---
+++ ObjectsAndClases/Exercise/OrderByAge/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OrderByAge
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            List<Order> peoples = new List<Order>();
+
+            while (input[0]!= "End")
+            {
+                Order newMen = new Order(input[0], int.Parse(input[1]), int.Parse(input[2]));
+                peoples.Add(newMen);
+
+                input = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .ToArray();
+            }
+           // peoples.OrderBy(x=>x.Age);
+            Console.WriteLine(string.Join("",peoples.OrderBy(x => x.Age)));
+            
+
+
+        }
+
+    }
+    class Order
+    {
+        public Order(string name, int number, int age)
+        {
+            Name = name;
+            Number = number;
+            Age = age;
+        }
+        public string  Name { get; set; }
+        public int Number { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Name} with ID: {Number} is {Age} years old.");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ObjectsAndClases/Exercise/Students/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Students> stud = new List<Students>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] comand = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+               //double gr = double.Parse(input[2])
+                Students student = new Students(comand[0], 
+                    comand[1], double.Parse(comand[2]));
+                stud.Add(student);
+
+
+            }
+            stud = stud.OrderByDescending(x => x.Grade).ToList();
+
+            foreach (Students student in stud)
+            {
+                Console.WriteLine(student);
+            }
+
+
+        }
+    }
+    class Students
+    {
+        public Students(string first, string last, double x)
+        {
+            FirstName = first;
+            LastName = last;
+            Grade = x;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} {Grade:F2}";
+        }
+    }
+}
