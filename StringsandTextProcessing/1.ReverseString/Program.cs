1fda85737c9707332e0699f5a79ec70c1a250071
Georgi Pangev <PangevG@Gmail.com>
2021-03-31 17:26:52 +0300

    String and text added.

---
+++ StringsandTextProcessing/1.ReverseString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string reverse = "";
+            while (input != "end")
+            {
+
+                //input = Console.ReadLine();
+
+
+
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    //string n = input[i].Reverse
+                    reverse += input[i];
+                }
+                Console.WriteLine($"{input} = {reverse}");
+                reverse = "";
+                input = Console.ReadLine();
+               
+            }
+           
+
+        }
+    }
+}
---
+++ StringsandTextProcessing/Digits,LettersandOther/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _5.Digits_LettersandOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder chars = new StringBuilder();
+            StringBuilder digits = new StringBuilder();
+            StringBuilder others = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    digits.Append(text[i]);
+                }
+                else if (char.IsLetter(text[i]))
+                {
+                    chars.Append(text[i]);
+                }
+                else
+                {
+                    others.Append(text[i]);
+                }
+            }
+            Console.WriteLine(digits.ToString());
+            Console.WriteLine(chars.ToString());
+            Console.WriteLine(others.ToString());
+
+        }
+    }
+}
