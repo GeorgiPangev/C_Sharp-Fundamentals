ed30874193600cf270c6c82d27e1c5d5e2067ae0
Georgi Pangev <PangevG@Gmail.com>
2021-04-02 23:46:23 +0300

    Two More added.

---
+++ StringsandTextProcessing/BanedWord/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace BanedWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] baned = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string text = Console.ReadLine();
+            //StringBuilder text = new StringBuilder(textOne.Length);
+            //text.Append(textOne);
+
+            foreach (var word in baned)
+            {
+                while (text.Contains(word))
+                {
+                    string n = new string('*', word.Length);
+                    text = text.Replace(word, new string('*', word.Length));
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ StringsandTextProcessing/RepetedStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace RepetedStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            StringBuilder repeate = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+
+
+
+                for (int j = 0; j < input[i].Length; j++)
+                {
+
+                    repeate.Append(input[i]);
+                }
+           }
+            Console.WriteLine(repeate.ToString());
+        }
+    }
+}
