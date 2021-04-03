436ea02c78db4a4362a0cb142484a6fe50cf67e6
Georgi Pangev <PangevG@Gmail.com>
2021-04-03 20:17:49 +0300

    Finale One.

---
+++ StringsandTextProcessing/Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string toRem = Console.ReadLine().ToLower();
+            string input = Console.ReadLine().ToLower();
+
+           
+            while (input.Contains(toRem))
+            {
+                    int n = input.IndexOf(toRem) ;
+                    input = input.Remove(n, toRem.Length);
+             }
+            
+            Console.WriteLine(input);
+        }
+    }
+}
