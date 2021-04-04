dac793d4ef56eb5dd9da08c6259b15fe45cbc2d9
Georgi Pangev <PangevG@Gmail.com>
2021-04-04 19:45:49 +0300

    exrcise folred

---
+++ AssociatetArrays/Exercise/CompanyeUsers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompanyeUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" -> ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            SortedDictionary<string, List<string>> companis = 
+                new SortedDictionary<string, List<string>>();
+
+
+            while (input[0] != "End")
+            {
+                
+                if (companis.ContainsKey(input[0]) == false)
+                {
+                    companis.Add(input[0], new List<string> { input[1] });
+                }
+                else if (companis[input[0]].Contains(input[1]) == false)
+                {
+                    companis[input[0]].Add(input[1]);
+                }
+
+                input = Console.ReadLine()
+                .Split(" -> ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+               
+            }
+            foreach (var c in companis)
+            {
+                Console.WriteLine(c.Key);
+                for (int i = 0; i < c.Value.Count; i++)
+                {
+                    Console.WriteLine($"-- {c.Value[i]}");
+                }
+            }
+        }
+    }
+}
---
+++ AssociatetArrays/Exercise/CursesInformation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CursesInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> curses = new Dictionary<string, List<string>>();
+
+            string[] input = Console.ReadLine()
+                 .Split(" : ", StringSplitOptions.RemoveEmptyEntries)
+                 .ToArray();
+
+            while (input[0]!= "end")
+            {
+                string curentCurs = input[0];
+                string curentStudent = input[1];
+                if (curses.ContainsKey(curentCurs)== false)
+                {
+                    curses.Add(curentCurs, new List<string> { curentStudent });
+                }
+                else if (curses.ContainsKey(curentCurs))
+                {
+                    if (curses[curentCurs].Contains(curentStudent) == false)
+                    {
+                        curses[curentCurs].Add(curentStudent);
+                    }
+                }
+                input = Console.ReadLine()
+                 .Split(" : ", StringSplitOptions.RemoveEmptyEntries)
+                 .ToArray();
+            }
+            
+            foreach (var curs in curses.OrderByDescending(x=>x.Value.Count))
+            {
+                Console.WriteLine($"{curs.Key}: {curs.Value.Count}");
+                // foreach (var student in curs.Value.OrderBy(x=>x))
+                //
+                
+                    Console.WriteLine($"{string.Join(Environment.NewLine,curs.Value)}");
+                //}
+
+            }
+
+
+
+        }
+    }
+}
