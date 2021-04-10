86cc8eba2f66b8367f0186c0b2d412d21eb81192
Georgi Pangev <PangevG@Gmail.com>
2021-04-10 20:33:33 +0300

    More added.

---
+++ ForLoops/Exercise/Hosptal/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Hosptal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double days = Double.Parse(Console.ReadLine());
+
+            Double doc = 7;
+            Double ptchek = 0;
+            Double ptunchek = 0;
+            Double unchektwoDays = 0;
+
+            for (Double i = 1; i <= days; i++)
+            {
+                Double pt = Double.Parse(Console.ReadLine());
+                if (i % 3 == 0 && unchektwoDays > 0)
+                {
+                    doc += 1;
+                    unchektwoDays = 0;
+                }
+
+                if (doc <= pt)
+                {
+                    ptchek += doc;
+                    ptunchek += pt - doc;
+                    unchektwoDays += ptunchek;
+                }
+                else if (doc > pt)
+                {
+                    ptchek += pt;
+                }
+            }       
+            Console.WriteLine($"Treated patients: {ptchek}.");
+            Console.WriteLine($"Untreated patients: {ptunchek}.");
+        }
+    }
+}
---
+++ ForLoops/Exercise/Lili/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lili
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double ages = double.Parse(Console.ReadLine());
+            double washingMashinPrice = double.Parse(Console.ReadLine());
+            double toyPrice = double.Parse(Console.ReadLine());
+
+            double evenBurthdays = 0;
+            double evenmonicont = 0;
+            double oddBurthdays = 0;
+
+            for (int i = 1; i <= ages; i++)
+            {
+                if (i %2==0)
+                {
+                    evenBurthdays += 1;
+                    evenmonicont = evenmonicont + (evenBurthdays * 10); 
+
+                }
+                else if (i %2 != 0)
+                {
+                    oddBurthdays += 1;
+                }
+            }
+            double evenSum = evenmonicont - evenBurthdays;
+            double oddsum = oddBurthdays * toyPrice;
+            double finSum = evenSum + oddsum;
+            if (washingMashinPrice <= finSum)
+            {
+                Console.WriteLine($"Yes! {finSum - washingMashinPrice :F2}");
+            }
+            else if (washingMashinPrice > finSum)
+            {
+                Console.WriteLine($"No! {Math.Abs(washingMashinPrice - finSum):F2}");
+            }
+          
+
+
+        }
+    }
+}
