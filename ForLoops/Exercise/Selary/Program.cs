dab6a0da176fe47eec5e84dda6828563c27a9b0c
Georgi Pangev <PangevG@Gmail.com>
2021-04-13 14:16:52 +0300

    Last two added.

---
+++ ForLoops/Exercise/Selary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _7Selary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double tabs = Double.Parse(Console.ReadLine());
+            Double salary = Double.Parse(Console.ReadLine());
+
+            for (double i = 1; i <= tabs; i++)
+            {
+                if (salary <= 0)
+                {
+                    tabs = 0;
+                    Console.WriteLine("You have lost your salary.");
+                }
+                string sites = Console.ReadLine();
+                switch (sites)
+                    {
+                        case "Facebook": salary -= 150; break;
+                        case "Instagram": salary -= 100; break;
+                        case "Reddit": salary -= 50; break;
+                    }    }  
+            if (salary > 0)
+            {
+                Console.WriteLine(Math.Round(salary));
+            }            
+        }
+    }
+}
