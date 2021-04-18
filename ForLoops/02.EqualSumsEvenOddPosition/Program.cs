27a7dfb6faf690efd357254ef0e716d06645b979
Georgi Pangev <PangevG@Gmail.com>
2021-04-18 22:14:58 +0300

    ++

---
+++ ForLoops/02.EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _02.EqualSumsEvenOddPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumb = int.Parse(Console.ReadLine());
+            int secNumb = int.Parse(Console.ReadLine());
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = firstNumb; i <= secNumb; i++)
+            {
+               
+                string courent = i.ToString();
+                for (int j = 0; j < courent.Length; j++)
+                {
+                    int digit = int.Parse(courent[j].ToString());
+
+                    if (j %2 == 0)
+                    {
+                        evenSum += digit;
+                    }
+                    else
+                    {
+                        oddSum += digit;
+                    }
+                }
+                if (evenSum == oddSum)
+                {
+                    Console.Write(i + " ");
+                }
+                evenSum = 0;
+                oddSum = 0;
+
+            }
+
+        }
+    }
+}
---
+++ ForLoops/03.SumPrimeNonPrime/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _03.SumPrimeNonPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double n = Double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            double n = 0;
+
+            double primeSum = 0;
+            Double nonPrimeSum = 0;
+            bool isStop = false;
+            bool nonPrime = false;
+
+            while (input != "stop")
+            {
+                n = Double.Parse(input);
+                if (n==1 || n == 0)
+                {
+                    nonPrimeSum += n;
+                }
+                else if (n==2)
+                {
+                    primeSum += n;
+                }
+                else if (n>2)
+                {
+                    for (int i = 2; i < n; i++)
+                    {
+                        if (n%i == 0)
+                        {
+                            nonPrime = true; ;
+                        }
+                    }
+                    if (!nonPrime)
+                    {
+                        primeSum += n;
+                    }
+                    else if (nonPrime)
+                    {
+                        nonPrimeSum += n;
+                        nonPrime = false;
+                    }
+                }
+                else if (n<0)
+                {
+                    Console.WriteLine("Number is negative.");
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {primeSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
+
+
+        }
+    }
+}
