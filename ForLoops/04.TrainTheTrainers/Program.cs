3672088a3168132bf22363e85c59a6d54c61fb36
Georgi Pangev <PangevG@Gmail.com>
2021-04-19 20:33:57 +0300

    Plus two.

---
+++ ForLoops/04.TrainTheTrainers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04.TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double corentSum = 0;
+            double allSum = 0;
+            double counterPres = 0;
+            bool isFinish = false;
+
+            while (!isFinish)
+            {
+                string pres = Console.ReadLine();
+                if (pres == "Finish" )
+                {
+                    break;
+                }
+                for (int i = 1; i <= n; i++)
+                {
+                    string input = Console.ReadLine();
+                    if (input == "Finish")
+                    {
+                        isFinish = true; break;
+                    }
+                    double greid = double.Parse(input);
+                    counterPres++;
+                    corentSum += greid;
+                    allSum += greid;
+                }
+                Console.WriteLine($"{pres} - {(corentSum/ n):F2}.");
+                corentSum = 0;
+            }
+            Console.WriteLine($"Student's final assessment is {(allSum/ counterPres):F2}.");
+
+        }
+    }
+}
---
+++ ForLoops/4.MajicNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _4.MajicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double mN = double.Parse(Console.ReadLine());
+
+            bool isFound = false;
+            double counter = 0;
+            double first = 0;
+            double sec = 0;
+            for (double x1 = x; x1 <= y; x1++)
+            {
+                for (double y1 = x; y1 <= y; y1++)
+                {
+                    counter++;
+                    if (x1 + y1 == mN)
+                    {
+                        first = x1;
+                        sec = y1;
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (isFound)
+                {
+                    break;
+                }
+            }
+            if (isFound)
+            {
+                Console.WriteLine($"Combination N:{counter} ({first} + {sec} = {mN})");
+            }
+            else if (!isFound)
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {mN}");
+            }
+        }
+    }
+}
