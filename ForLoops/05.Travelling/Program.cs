60a31e19b5693386ea680e887a57ea7f9b146175
Georgi Pangev <PangevG@Gmail.com>
2021-04-20 21:08:38 +0300

    ++

---
+++ ForLoops/05.Travelling/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _05.Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dest = Console.ReadLine();
+            double price = double.Parse(Console.ReadLine());
+
+            double incomCount = 0;
+            bool isenof = false;
+            bool breked = false;
+            double sumneeded = 0;
+
+            while (dest != "End")
+            {
+                if (breked)
+                {
+                    break;
+                }
+                if (isenof)
+                {
+                    dest = Console.ReadLine();
+                    if (dest == "End")
+                    {
+                        
+                        break;
+                    }
+                    price = double.Parse(Console.ReadLine());
+                    incomCount = 0; isenof = false;
+                }
+                string inc = Console.ReadLine();
+                double incom = double.Parse(inc);
+                incomCount+= incom;
+                for (double i = 0; i < price; i+= incom)
+                {
+
+                   
+                   
+                    if (incomCount>= price)
+                    {
+                        Console.WriteLine($"Going to {dest}!");
+                        isenof = true;
+                        break;
+                    }
+                    inc = Console.ReadLine();
+                    if (inc == "End")
+                    {
+                        breked = true;
+                        break;
+                    }
+                    incom = double.Parse(inc);
+                    incomCount += incom;
+
+                }
+            }
+        }
+    }
+}
---
+++ ForLoops/05.Travelling2.0/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.Travelling2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String dest = Console.ReadLine();
+            double cost = double.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            while (dest != "End")
+            {
+                
+                while (cost>= sum)
+                {
+                    double incom = double.Parse(Console.ReadLine());
+
+                    sum += incom;
+                }
+                sum = 0;
+                Console.WriteLine($"Going to {dest}!");
+
+                dest = Console.ReadLine();
+                if (dest == "End")
+                { break; }
+                cost = double.Parse(Console.ReadLine());
+
+            }
+        }
+    }
+}
