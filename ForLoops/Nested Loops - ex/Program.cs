fa383895e0a69997a168570627f4e78a1c65b798
Georgi Pangev <PangevG@Gmail.com>
2021-04-27 20:46:51 +0300

    Last problem

---
+++ ForLoops/Nested Loops - ex/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Nested_Loops___ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int corent = 1;
+            bool biger = false;
+
+            for (int rol = 1;  rol<= n; rol ++)
+            {
+                for (int col = 1; col <= rol; col++)
+                {
+                    if ( corent> n)
+                    {
+                        biger = true;
+                        break;
+                    }
+                    Console.Write(corent + " ");
+                    corent++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
